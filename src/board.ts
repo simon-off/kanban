8d2a233da9a6fd462d7d89f83c83d90f67e8e687
simon-off <simon-off@users.noreply.github.com>
2024-05-28 19:37:59 +0200

    Updated board logic

--- src/board.ts
+++ src/board.ts
@@ -3,35 +3,20 @@ type Card = {
   description?: string;
 };
 
-type Column = {
-  title: string;
-  cards: Card[];
-};
-
-type Board = {
-  columns: Column[];
-};
+type Board = Record<string, Card[]>;
 
 const board: Board = {
-  columns: [
+  todo: [
+    { title: "Fix stuff", description: "Very important stuff!" },
+    { title: "Get stuff done", description: "yes this is stuff" },
     {
-      title: "Todo",
-      cards: [
-        { title: "Fix stuff", description: "Very important stuff!" },
-        { title: "Get stuff done", description: "yes this is stuff" },
-        {
-          title: "Update thing",
-          description:
-            "long description here for all the things that needs updating. Perhaps this need to be truncated?",
-        },
-        { title: "Finish task", description: "Finish it!" },
-      ],
+      title: "Update thing",
+      description:
+        "long description here for all the things that needs updating. Perhaps this need to be truncated?",
     },
-    {
-      title: "In Progress",
-      cards: [],
-    },
+    { title: "Finish task", description: "Finish it!" },
   ],
+  "in-progress": [],
 };
 
 function classify(input: string): string {
@@ -39,88 +24,84 @@ function classify(input: string): string {
 }
 
 function drawCard(card: Card, columnId: string) {
-  return /*html*/ `
-    <article class="card" draggable="true" id="${classify(
-      card.title
-    )}" data-column-id="${columnId}">
-      <header>
-        <h3>${card.title}</h3>
-      </header>
-      <div class="body">
-        ${card.description ? /*html*/ `<p>${card.description}</p>` : ``}
-      </div>
-    </article>
+  const cardEl = document.createElement("article");
+  cardEl.classList.add("card");
+  cardEl.draggable = true;
+  cardEl.id = classify(card.title);
+  cardEl.dataset.columnId = columnId;
+  cardEl.innerHTML = /*html*/ `
+    <header>
+      <h3>${card.title}</h3>
+    </header>
+    <div class="body">
+      ${card.description ? /*html*/ `<p>${card.description}</p>` : ``}
+    </div>
   `;
+
+  cardEl.addEventListener("dragstart", (ev: DragEvent) => {
+    if (!ev.dataTransfer) return;
+
+    console.log("hallå");
+
+    ev.dataTransfer.setData("text/plain", cardEl.id);
+  });
+
+  return cardEl;
 }
 
-function drawColumn(column: Column) {
-  return /*html*/ `
-    <section class="column" id="${classify(column.title)}">
-      <h2>${column.title}</h2>
-      <div class="cards">${column.cards
-        .map((card) => drawCard(card, classify(column.title)))
-        .join("")}</div>
-    </section>
+function drawColumn(key: string, cards: Card[]) {
+  const columnEl = document.createElement("section");
+  columnEl.classList.add("column");
+  columnEl.id = classify(key);
+  columnEl.innerHTML = /*html*/ `
+    <h2>${key}</h2>
+    <div class="cards"></div>
   `;
+
+  columnEl
+    .querySelector(".cards")
+    ?.append(...cards.map((card) => drawCard(card, classify(key))));
+
+  columnEl.addEventListener("dragover", (ev) => {
+    if (!ev.dataTransfer) return;
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+  });
+
+  columnEl.addEventListener("drop", (ev: DragEvent) => {
+    if (ev.dataTransfer === null) return;
+
+    ev.preventDefault();
+    const cardId = ev.dataTransfer.getData("text/plain");
+    const cardEl = document.getElementById(cardId);
+
+    const oldColumn = board[cardEl?.dataset.columnId ?? ""];
+    const newColumn =
+      board[(ev.target as HTMLElement)?.querySelector("h2")?.innerText ?? ""];
+    const card = oldColumn.find((card) => classify(card.title) === cardId);
+
+    if (!oldColumn || !newColumn || !card) return;
+
+    newColumn?.push(card);
+    oldColumn?.splice(oldColumn.indexOf(card), 1);
+
+    const mainEl = document.querySelector<HTMLDivElement>("main");
+
+    if (!mainEl) return;
+    mainEl.innerHTML = "";
+    mainEl.appendChild(drawBoard());
+  });
+
+  return columnEl;
 }
 
 export function drawBoard(): HTMLDivElement {
   const boardEl = document.createElement("div");
   boardEl.classList.add("board");
 
-  const sectionsEl = board.columns.map(drawColumn).join("");
-  boardEl.innerHTML = sectionsEl;
-
-  const cards = boardEl.querySelectorAll<HTMLDivElement>(".card");
-
-  for (const card of cards) {
-    card.addEventListener("dragstart", (ev: DragEvent) => {
-      if (!ev.dataTransfer) return;
-
-      ev.dataTransfer.setData("text/plain", card.id);
-    });
-  }
-
-  const columns = boardEl.querySelectorAll<HTMLElement>(".column");
-
-  for (const column of columns) {
-    column.addEventListener("dragover", (ev) => {
-      if (!ev.dataTransfer) return;
-
-      ev.preventDefault();
-      ev.dataTransfer.dropEffect = "move";
-    });
-
-    column.addEventListener("drop", (ev: DragEvent) => {
-      if (ev.dataTransfer === null) return;
-
-      ev.preventDefault();
-      const cardId = ev.dataTransfer.getData("text/plain");
-      const cardEl = document.querySelector<HTMLDivElement>(`#${cardId}`);
-
-      const oldColumn = board.columns.find(
-        (x) => classify(x.title) === cardEl?.dataset.columnId
-      );
-      const card = oldColumn?.cards.find(
-        (card) => classify(card.title) === cardId
-      );
-      const newColumn = board.columns.find(
-        (x) => classify(x.title) === (ev.target as HTMLElement)?.id
-      );
-
-      if (!card) return;
-
-      newColumn?.cards.push(card);
-      oldColumn?.cards.splice(oldColumn.cards.indexOf(card), 1);
-
-      console.log(board);
-      const mainEl = document.querySelector<HTMLDivElement>("main");
-
-      if (!mainEl) return;
-      mainEl.innerHTML = "";
-      mainEl.appendChild(drawBoard());
-    });
-  }
+  boardEl.append(
+    ...Object.entries(board).map(([key, value]) => drawColumn(key, value))
+  );
 
   return boardEl;
 }
